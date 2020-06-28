dbd79b92a3c549548f34b550406e49f98fe98f8a
lucaswerner90 <wernerlucas12@gmail.com>
2020-06-27 23:39:15 -0300

    Change page title

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -28,7 +28,7 @@ export default function MyApp(props: AppProps) {
   return (
     <React.Fragment>
       <Head>
-        <title>Lucas Werner Development</title>
+        <title>LucianaAbraoStudio.</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap" rel="stylesheet"/>
         <link rel='stylesheet' type='text/css' href='/css/nprogress.css' />

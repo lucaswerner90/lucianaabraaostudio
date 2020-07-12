0c1a4498bd2f3c0493e1c989a961b918b9399e4e
lucaswerner90 <wernerlucas12@gmail.com>
2020-07-12 18:48:39 -0300

    Lets try adding cors to POST api route

--- frontend/src/pages/api/product-request/send.ts
+++ frontend/src/pages/api/product-request/send.ts
@@ -1,6 +1,28 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import shopSendEmail from '../email/shopSendEmail';
 import API_AXIOS from '../../../API_AXIOS';
+import Cors from 'cors';
+
+function initMiddleware(middleware: any) {
+	return (req: any, res: any) =>
+		new Promise((resolve, reject) => {
+			middleware(req, res, (result: any) => {
+				if (result instanceof Error) {
+					return reject(result);
+				}
+				return resolve(result);
+			})
+		})
+}
+
+const cors = initMiddleware(
+	// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+	Cors({
+		// Only allow requests with GET, POST and OPTIONS
+		methods: ['GET', 'POST', 'OPTIONS'],
+	})
+)
+
 
 const createProductRequest = async (design: string, name: string, email: string, description: string) => {
 	const body = {
@@ -14,6 +36,7 @@ const createProductRequest = async (design: string, name: string, email: string,
 }
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+	await cors(req, res);
 	const { body } = req;
 	try {
 		const requestID = await createProductRequest(body.product.id, body.name, body.email, body.description);

af7c7d6d272627bb2ed55788264f4a54a058ea5d
lucaswerner90 <wernerlucas12@gmail.com>
2020-07-13 17:29:30 -0300

    Frontend build fix

--- frontend/src/pages/api/product-request/send.ts
+++ frontend/src/pages/api/product-request/send.ts
@@ -17,11 +17,8 @@ function initMiddleware(middleware: any) {
 
 const cors = initMiddleware(
 	// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-	Cors({
-		// Only allow requests with GET, POST and OPTIONS
-		methods: ['GET', 'POST', 'OPTIONS'],
-	});
-)
+	Cors({ methods: ['GET', 'POST', 'OPTIONS'] })
+);
 
 
 const createProductRequest = async (design: string, name: string, email: string, description: string) => {
